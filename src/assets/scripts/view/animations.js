1320825494495d5e8c6b441e8f60980f6167b702
Maximiliano Minetto <maximinetto@gmail.com>
2022-02-08 12:43:49 -0300

    fix banner

--- src/assets/scripts/view/animations.js
+++ src/assets/scripts/view/animations.js
@@ -2,24 +2,11 @@ import rellax from "rellax";
 
 const elems = document.querySelectorAll(".parallax");
 const animations = document.querySelectorAll(".animation");
-let removeClasses = false;
 
-rellax(".parallax", {
-  horizontal: true,
-  callback: function (position) {
-    if (!removeClasses) {
-      for (let i = 0; i < elems.length; i++) {
-        const elem = elems[i];
-        elem.classList.remove(
-          "fade-in-left",
-          "fade-in-right",
-          "fade-in-fwd",
-          "slide-in-elliptic-bottom"
-        );
-      }
-    }
-    removeClasses = true;
-  },
+removeAnimation().then(() => {
+  rellax(".parallax", {
+    horizontal: true,
+  });
 });
 
 for (let i = 0; i < animations.length; i++) {
@@ -31,11 +18,31 @@ for (let i = 0; i < animations.length; i++) {
   });
 }
 
+async function removeAnimation() {
+  for (let i = 0; i < elems.length; i++) {
+    const elem = elems[i];
+    await promiseListener("animationend", elem, { once: true });
+    elem.classList.remove(
+      "fade-in-left",
+      "fade-in-right",
+      "fade-in-fwd",
+      "slide-in-elliptic-bottom-fwd"
+    );
+  }
+}
+
+async function promiseListener(type, elem, options) {
+  return new Promise((resolve, reject) => {
+    elem.addEventListener(type, resolve, options);
+  });
+}
+
 function animate(classname, target, options) {
   const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       if (entry.intersectionRatio > 0) {
         entry.target.classList.add(classname);
+        observer.unobserve(entry.target);
       } else {
         entry.target.classList.remove(classname);
       }
